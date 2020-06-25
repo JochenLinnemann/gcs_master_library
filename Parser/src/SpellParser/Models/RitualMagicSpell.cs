69530d39f9086c544a4c042ad0c562b8e6eb6db1
Jochen Linnemann <jl@jochenlinnemann.de>
2020-06-25 23:57:12 +0200

    Add translation and magic school skills

--- Parser/src/SpellParser/Models/RitualMagicSpell.cs
+++ Parser/src/SpellParser/Models/RitualMagicSpell.cs
@@ -5,16 +5,50 @@ namespace SpellParser.Models
 {
     public class RitualMagicSpell
     {
+        #region Constructors
+
+        public RitualMagicSpell()
+        {
+        }
+
+        public RitualMagicSpell(RitualMagicSpell spell)
+        {
+            Name = spell.Name;
+            Difficulty = spell.Difficulty;
+            College = spell.College;
+            PowerSource = spell.PowerSource;
+            SpellClass = spell.SpellClass;
+            CastingCost = spell.CastingCost;
+            MaintenanceCost = spell.MaintenanceCost;
+            CastingTime = spell.CastingTime;
+            Duration = spell.Duration;
+            Points = spell.Points;
+            Reference = spell.Reference;
+            BaseSkill = spell.BaseSkill;
+            PrereqCount = spell.PrereqCount;
+            Categories = new List<string>(spell.Categories);
+            MeleedWeapons = new List<MeleeWeapon>(spell.MeleedWeapons);
+            RangedWeapons = new List<RangedWeapon>(spell.RangedWeapons);
+            Version = spell.Version;
+        }
+
+        #endregion
+
+        // @formatter:off — disable formatter after this line
+
         #region Properties
 
-        [XmlElement("points")]
-        public string Points { get; set; }
+        [XmlElement("name")]
+        public string Name { get; set; }
 
-        [XmlElement("reference")]
-        public string Reference { get; set; }
+        [XmlElement("name_de")]
+        public string NameDe { get; set; }
 
-        [XmlElement("base_skill")]
-        public string BaseSkill { get; set; }
+        [XmlElement("difficulty")]
+        public string Difficulty { get; set; }
+
+        [XmlElement("college")]
+        public string College { get; set; }
 
         [XmlElement("power_source")]
         public string PowerSource { get; set; }
@@ -30,35 +64,37 @@ namespace SpellParser.Models
 
         [XmlElement("casting_time")]
         public string CastingTime { get; set; }
+    
+        [XmlElement("duration")]
+        public string Duration { get; set; }
+
+        [XmlElement("points")]
+        public string Points { get; set; }
+
+        [XmlElement("reference")]
+        public string Reference { get; set; }
+     
+        [XmlElement("base_skill")]
+        public string BaseSkill { get; set; }
+      
+        [XmlElement("prereq_count")]
+        public int PrereqCount { get; set; }
 
         [XmlArray("categories")]
         [XmlArrayItem("category")]
         public List<string> Categories { get; set; }
 
-        [XmlElement("name")]
-        public string Name { get; set; }
-
-        [XmlElement("difficulty")]
-        public string Difficulty { get; set; }
-
-        [XmlElement("college")]
-        public string College { get; set; }
-
-        [XmlElement("duration")]
-        public string Duration { get; set; }
-
-        [XmlElement("prereq_count")]
-        public int PrereqCount { get; set; }
+        [XmlElement("melee_weapon")]
+        public List<MeleeWeapon> MeleedWeapons { get; set; }
 
         [XmlElement("ranged_weapon")]
         public List<RangedWeapon> RangedWeapons { get; set; }
 
-        [XmlElement("melee_weapon")]
-        public List<MeleeWeapon> MeleedWeapons { get; set; }
-
         [XmlAttribute("version")]
         public string Version { get; set; }
 
-        #endregion
+         #endregion
+
+        // @formatter:on — enable formatter after this line
     }
 }
--- Parser/src/SpellParser/Program.cs
+++ Parser/src/SpellParser/Program.cs
@@ -1,8 +1,11 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Xml.Serialization;
 using CsvHelper;
-using CsvHelper.Configuration;
+using Newtonsoft.Json;
 using SpellParser.Models;
 
 namespace SpellParser
@@ -12,6 +15,11 @@ namespace SpellParser
         #region Static Fields
 
         private const string SpellsResourceName = "SpellParser.data.Ritual Magic Spells.spl";
+        private const string TranslatorApiKeyResourceName = "SpellParser.translator-api-key.txt";
+
+        private static readonly Dictionary<string, string> _enToDe = new Dictionary<string, string>();
+
+        private static string _translatorApiKey;
 
         #endregion
 
@@ -25,18 +33,151 @@ namespace SpellParser
             var xmlSerializer = new XmlSerializer(typeof(SpellList));
             var spellList = xmlSerializer.Deserialize(spellsResouceStream) as SpellList ?? new SpellList();
 
-            var csvWriter = new CsvWriter(
-                new StreamWriter("C:\\Temp\\Ritual Magic Spells-Adapted.csv"),
-                new CsvConfiguration { Delimiter = ";" });
+            _translatorApiKey =
+                new StreamReader(assembly.GetManifestResourceStream(TranslatorApiKeyResourceName) ?? Stream.Null)
+                    .ReadToEnd();
 
-            csvWriter.WriteHeader(new { Name = "", College = "", PrereqCount = 0 }.GetType());
+            spellList.Spells = SeparateByCollege(spellList, true, true).ToList();
 
+            using var streamWriter = new StreamWriter("C:\\Temp\\Ritual Magic Spells-Adapted.csv");
+            using var csvWriter = new CsvWriter(streamWriter, CultureInfo.InvariantCulture);
+
+            csvWriter.Configuration.Delimiter = ";";
+            csvWriter.WriteRecords(spellList.Spells);
+
+            csvWriter.Flush();
+        }
+
+        private static IEnumerable<RitualMagicSpell> SeparateByCollege(SpellList spellList,
+            bool useMagicSchoolsOfSplittermond, bool translateToGerman)
+        {
             foreach (var spell in spellList.Spells)
-            foreach (var college in spell.College.Split('/').Select(c => c.Trim()))
-                csvWriter.WriteRecord(new
+            {
+                var colleges = spell.College
+                    .Replace("Technological/", "Technological+")
+                    .Replace("Meta/Linking", "Meta+Linking").Split("/")
+                    .Select(c => c.Trim()
+                        .Replace("Technological+", "Technological/")
+                        .Replace("Meta+Linking", "Meta/Linking"));
+
+                foreach (var college in colleges)
+                    yield return new RitualMagicSpell(spell)
+                    {
+                        Name = $"{spell.Name} ({GetBaseSkill(college)})",
+                        NameDe = $"{Translate(spell.Name)} ({GetBaseSkill(college)})",
+                        College = college,
+                        BaseSkill = GetBaseSkill(college)
+                    };
+            }
+
+            string GetBaseSkill(string college)
+            {
+                if (!useMagicSchoolsOfSplittermond)
+                    return
+                        $"Path of {college.Replace("Technological", "Technology").Replace("Meta", "Meta-Spells").Replace("Armor Enchantment", "Enchantment")}";
+
+                switch (college)
                 {
-                    Name = spell.Name + " (" + college + ")", college, spell.PrereqCount
-                });
+                    case "Air":
+                        return "Windmagie";
+                    case "Animal":
+                        return "Tiermagie";
+                    case "Body Control":
+                        return "Verwandlungsmagie";
+                    case "Communication & Empathy":
+                        return "Kommunikationsmagie";
+                    case "Earth":
+                    case "Technological/Metal":
+                        return "Felsmagie";
+                    case "Fire":
+                        return "Feuermagie";
+                    case "Food":
+                        return "Nahrungsmagie";
+                    case "Healing":
+                        return "Heilungsmagie";
+                    case "Illusion & Creation":
+                        return "Illusionsmagie";
+                    case "Knowledge":
+                        return "Erkenntnismagie";
+                    case "Light & Darkness":
+                        return "Licht- & Schattenmagie";
+                    case "Making & Breaking":
+                        return "Stärkungsmagie";
+                    case "Meta":
+                    case "Meta/Linking":
+                        return "Metamagie";
+                    case "Mind Control":
+                        return "Beherrschungsmagie";
+                    case "Movement":
+                        return "Bewegungsmagie";
+                    case "Necromancy":
+                        return "Todesmagie";
+                    case "Plant":
+                        return "Pflanzenmagie";
+                    case "Protection":
+                        return "Schutzmagie";
+                    case "Sound":
+                        return "Geräuschmagie";
+                    case "Water":
+                        return "Wassermagie";
+                    case "Weather":
+                        return "Wettermagie";
+
+                    case "Gate":
+                        return "Portalmagie";
+
+                    case "Enchantment":
+                    case "Armor Enchantment":
+                        return "Artefaktmagie";
+
+                    case "Technological":
+                    case "Technological/Energy":
+                    case "Technological/Machine":
+                    case "Technological/Plastic":
+                    case "Technological/Radiation":
+                        return "Technomagie";
+
+                    default:
+                        return college;
+                }
+            }
+
+            string Translate(string text)
+            {
+                if (!translateToGerman)
+                    return text;
+
+                if (_enToDe.ContainsKey(text))
+                    return _enToDe[text];
+
+                var request =
+                    WebRequest.Create(
+                        "https://api.cognitive.microsofttranslator.com/translate?api-version=3.0&from=en&to=de");
+                request.Method = "POST";
+
+                request.Headers.Add("Ocp-Apim-Subscription-Key", _translatorApiKey);
+                request.Headers.Add("Ocp-Apim-Subscription-Region", "westeurope");
+                request.ContentType = "application/json; charset=utf-8";
+
+                var writer = new StreamWriter(request.GetRequestStream());
+                writer.Write(JsonConvert.SerializeObject(new[] { new { Text = text } }));
+                writer.Flush();
+
+                using var response = request.GetResponse();
+                var resultData = JsonConvert.DeserializeAnonymousType(
+                    new StreamReader(response.GetResponseStream() ?? Stream.Null).ReadToEnd(), new[]
+                    {
+                        new
+                        {
+                            translations = new[]
+                            {
+                                new { text = "" }
+                            }
+                        }
+                    });
+
+                return _enToDe[text] = resultData?[0].translations?[0].text;
+            }
         }
 
         #endregion
